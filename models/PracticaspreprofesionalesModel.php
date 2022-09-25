3dcd70b0258de6f4b1079ff484a0f0bf55fd271e
josuelopez <tshadez@hotmail.com>
2022-09-25 17:00:30 -0500

    nuevo query para obtener las horas

--- models/PracticaspreprofesionalesModel.php
+++ models/PracticaspreprofesionalesModel.php
@@ -12,8 +12,8 @@ class PracticaspreprofesionalesModel extends Mysql
     }
 
     public function selectPracticasPreProfesionales(){
-        $sql = "SELECT ppp.id_practica,CONCAT(al.cedula,' ',al.nombre,' ',al.apellido) as ced_nom_ape_al,
-                    CONCAT(em.ruc_empresa,' ',em.nombre_empresa,' ','(',pr.nombre,' ',pr.apellido,')') as ruc_nom_ape_pr_em,
+        $sql = "SELECT ppp.id_practica,CONCAT(al.cedula,'<br>',al.nombre,' ',al.apellido) as ced_nom_ape_al,
+                    CONCAT(em.ruc_empresa,'-',em.nombre_empresa,'<br>','(',pr.nombre,' ',pr.apellido,')') as ruc_nom_ape_pr_em,
                     ppp.fecha_inicio,ppp.fecha_fin,ppp.tipo_practica
         FROM Practicas_pre_profesionales as ppp
         INNER JOIN Alumnos as al ON al.id_alumno = ppp.id_alumno
@@ -57,6 +57,18 @@ class PracticaspreprofesionalesModel extends Mysql
                         or ep.nombre_representante like '%" . $this->str_search_profesor ."%' or ep.ruc_empresa like '%" . $this->str_search_profesor ."%' LIMIT 5" ;
         $request = $this->select_sql_all($sql);
         return $request;
+    }
+
+
+
+    public function selectAlumnoTotalHorasppp(int $str_search_alumno){
+        $this->str_search_alumno = $str_search_alumno;
+        $sql = "SELECT SUM(ppp.total_horas_ppp) as total_horas FROM Practicas_pre_profesionales as ppp
+                    INNER JOIN Alumnos as al ON al.id_alumno = ppp.id_alumno
+                    WHERE al.estado = 1 and ppp.estado = 1 and al.id_alumno = $this->str_search_alumno";
+        $request = $this->select_sql($sql);
+        return $request;
+
     }
 
 }
