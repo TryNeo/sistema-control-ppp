aa2e79917b6f55415d408a9644f33c8fda1968a3
josuelopez <tshadez@hotmail.com>
2022-10-10 16:20:27 -0500

    haciendo cambios de logica

--- assets/js/functions_historial_estudiante.js
+++ assets/js/functions_historial_estudiante.js
@@ -28,5 +28,64 @@ $(function(){
     ]
 
     const tableHistorialAlumno = configDataTables('.tableHistorialAlumno',base_url+"historial-estudiante/getHistorialEstudiante",columnData,columnDefs);
+    searchAlumno(tableHistorialAlumno);
+    pppEmpresarialReporte();
+});
 
-})
+function formatRepo (repo) {
+    if (repo.loading) {
+        return repo.text;
+    }
+
+    var option = $(
+        '<div class="wrapper">'
+            +'<p style="margin-bottom:12;">'
+                +'<b>Cedula: </b>'+repo.cedula+'<br>'
+                +'<b>Nombre y Apellido: </b>'+repo.text+'<br>'
+            +'</p>'
+        +'</div>'
+    )
+
+    return option;
+}
+
+
+function searchAlumno(tableHistorialAlumno){
+    $('#SearchAlumno').select2({
+        theme: "bootstrap-5",
+        language:'es',
+        allowClear:true,
+        ajax: {
+            url: base_url+"historial-estudiante/getSelectHistorialAlumno",
+            type: "POST",
+            dataType: 'json',
+            delay:250,
+            data: function (params) {
+                let queryParameters = {
+                    search: params.term
+                }
+                return queryParameters;
+            },
+            processResults: function (data) {
+                return {
+                    results: data
+                };
+            },
+            cache: true
+        },
+        placeholder:"Buscar alumno por cedula / nombre / apellido",
+        templateResult: formatRepo,
+    }).on('select2:select',function(e){
+        let data = e.params.data;
+        $('#SearchAlumno').val('');
+        $('#SearchAlumno').trigger('change.select2');
+        tableHistorialAlumno.ajax.url(base_url+"historial-estudiante/getHistorialEstudiante/"+data.id).load();
+    });
+}
+
+
+function pppEmpresarialReporte(){
+    $("#pppEmpresarial").click(function(){
+        abrir_modal_reporte('modalReporteEmpresarial');
+    })
+}
