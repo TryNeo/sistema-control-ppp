c4f08ef0f09c1ed0ad49b4e34d49b47ad2864576
josuelopez <tshadez@hotmail.com>
2022-10-10 16:24:44 -0500

    nuevo controllador

--- controllers/Reporte.php
+++ controllers/Reporte.php
@@ -8,25 +8,25 @@ class reporte extends fpdf{
 
     public function __construct(){
         parent::__construct();
-        $this->fontName = 'helvetica';
+        $this->fontName = 'times';
     }
     
     function renderHeader($title,$subtitle,$path_image,$position_x,$position_y,$width){
         $this->Image($path_image,$position_x,$position_y,$width);
         $this->SetTextColor(0, 0, 0);
-        $this->SetFont($this->fontName, 'B', 26);
-        $this->Cell(0, 80, utf8_decode($title), 0, 0,'C');
+        $this->SetFont($this->fontName, 'B', 15);
+        $this->Cell(0, 130, utf8_decode($title), 0, 0,'C');
         $this->SetY(12);
         $this->SetTextColor(51, 51, 51);
-        $this->SetFont($this->fontName, '', 9);
-        $this->Cell(0, 90, utf8_decode($subtitle), 0, 0,'C');
+        $this->SetFont($this->fontName, '', 12);
+        $this->Cell(305, 90, utf8_decode($subtitle), 0, 0,'C');
         $this->Ln();
     }
 
     function renderText($title) {
         $this->SetTextColor(51, 51, 51);
-        $this->SetY(70);
-        $this->SetFont($this->fontName, '', 17);
+        $this->SetY(85);
+        $this->SetFont($this->fontName, '', 12);
         $this->Cell(0, 12, utf8_decode($title), 0,0,'C');
         $this->Ln();
     }
@@ -35,7 +35,6 @@ class reporte extends fpdf{
     function Footer(){
         $this->SetY(-15);
         $this->SetFont('Arial','B',10);
-        $this->Cell(0,10,'Pagina '.$this->PageNo().'/{nb}',0,0,'C');
         $this->SetX(0);
     }
 
