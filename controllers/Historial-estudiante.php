7385c003818e791969797f8128940aaace67f3b1
josuelopez <tshadez@hotmail.com>
2022-10-23 16:22:27 -0500

    implementando nueva version para la cuestion de los reportes

--- controllers/Historial-estudiante.php
+++ controllers/Historial-estudiante.php
@@ -1,6 +1,7 @@
 <?php
 require_once("./libraries/core/controllers.php");
-require_once ("./controllers/Reporte.php");
+require_once ("./libraries/html2pdf/vendor/autoload.php");
+use Spipu\Html2Pdf\Html2Pdf;
 
 class Historialestudiante extends Controllers
 {
@@ -27,7 +28,6 @@ class Historialestudiante extends Controllers
         $this->views->getView($this,"historial-estudiante",$data);
     }
 
-
     public function getHistorialEstudiante($search_alumno = 0)
     {
         if (empty($_SESSION['permisos_modulo']['r'])) {
@@ -44,7 +44,6 @@ class Historialestudiante extends Controllers
         echo json_encode($data, JSON_UNESCAPED_UNICODE);
         die();
     }
-
 
     public function getSelectHistorialAlumno()
     {
@@ -78,88 +77,86 @@ class Historialestudiante extends Controllers
         die();
     }
 
-
-
-
-
     public function certificadopppempresarial($int_id_alumno = 0){
         if (empty($_SESSION['permisos_modulo']['r'])) {
             header('location:'.server_url.'Errors');
         }else{
             if($_SESSION['user_data']['id_rol'] != 2){
-                $pdf = new reporte();
-                $pdf->AliasNbPages();
-                $pdf->AddPage();
-                $pdf->SetTitle('');
-                $pdf->renderText('');
-                $pdf->SetFont('arial', '', 10);
-                $y = $pdf->GetY() + 8;
-                $pdf->Output('', 'certificado_ppp_empresarial_'.date("d_m_Y_H_i").'.pdf');
-            }else{
-                $pdf = new reporte();
-                $pdf->AliasNbPages();
-                $pdf->AddPage();
-                $pdf->SetTitle('Certificado practicas pre-profesionales');
-                $date_du_jour = fechaCastellano(date("d-m-Y"));
-                $pdf->renderHeader('CERTIFICADO DE PRACTICAS PRE-PROFESIONALES',$date_du_jour,'https://i.ibb.co/DQstGsn/favicon1.png',10,15,23);
-                $pdf->SetY(87);
-                $getAlumno= $this->model->selectCertificadoAlumno($_SESSION['user_data']['id_usuario']);
-                $mensaje = "Por medio de la presente certificamos que el estudiante ".$getAlumno['nombre_estudiante'].
-                " con cédula de identidad No. ".$getAlumno['cedula'].", de la carrera ".$getAlumno['nombre_carrera'].
-                ", ha cumplido con las prácticas siguientes prácticas pre-profesionales de tipo EMPRESARIAL:";
-                $pdf->Write(7, utf8_decode($mensaje));
-                $y = $pdf->GetY() + 13;
-
-                $pdf->SetXY(12, $y);
-                $pdf->MultiCell(50, 8, utf8_decode("Entidad Receptora"), 1, 'C');
-                
-                $pdf->SetXY(62, $y); 
-                $pdf->MultiCell(40, 8, utf8_decode("Tipo"), 1, 'C');
-
-                $pdf->SetXY(102, $y); 
-                $pdf->MultiCell(40, 8, utf8_decode("Horas Ejectudas"), 1, 'C');
-
-                $pdf->SetXY(142, $y); 
-                $pdf->MultiCell(26, 8, utf8_decode("Fecha inicio"), 1, 'C');
-
-                $pdf->SetXY(168, $y); 
-                $pdf->MultiCell(26, 8, utf8_decode("Fecha Fin"), 1, 'C');
-
-                $suma_total_horas=0;
-
-                $data = $this->model->selectCertificadoAlumnoDetalle($_SESSION['user_data']['id_usuario'],1);
-                foreach ($data as $value) {
-                    $y = $pdf->GetY();
-                    $pdf->SetXY(12, $y);
-                    $pdf->MultiCell(50,16, utf8_decode($value['nombre_empresa']), 1, 'J');
-                    $pdf->SetXY(62, $y);
-                    if ($value['tipo_practica'] == 1) {
-                        $pdf->MultiCell(40, 16, utf8_decode("EMPRESARIAL"), 1, 'J');
-                    }
-                    $pdf->SetXY(102, $y);
-                    $pdf->MultiCell(40, 16, utf8_decode($value['total_horas_ppp']), 1, 'C');
-                    $suma_total_horas+=$value['total_horas_ppp'];
-                    $pdf->SetXY(142, $y); 
-                    $pdf->MultiCell(26, 16, utf8_decode($value['fecha_inicio']), 1, 'C');
-
-                    $pdf->SetXY(168, $y); 
-                    $pdf->MultiCell(26, 16, utf8_decode($value['fecha_fin']), 1, 'C');
-    
+                $int_id_alumno = Intval(strclean($int_id_alumno));
+                if($int_id_alumno == 0){
+                    header('location:'.server_url.'Errors');
+                }else{
+                    $getAlumno= $this->model->selectCertificadoAlumno($int_id_alumno);
+                    $date_du_jour = fechaCastellano(date("d-m-Y"));
+                    $data[0] = $getAlumno;
+                    $data[0]['tipo']='EMPRESARIAL';
+                    $data[1] = $date_du_jour;
+                    $data[3] = $this->model->selectCertificadoAlumnoDetalle($int_id_alumno,1);
+                    $html2pdf = new Html2Pdf('P', 'A4', 'es', 'true', 'UTF-8');
+                    ob_end_clean();
+                    $html = getFile('template/reporte/reporte_ppp_general',$data);
+                    $html2pdf->setDefaultFont('times');
+                    $html2pdf->pdf->SetTitle('Certificado practicas pre-profesionales');
+                    $html2pdf->writeHTML($html);
+                    $html2pdf->output('certificado_ppp_empresarial_'.date("d_m_Y_H_i").'.pdf');
                 }
-                $y = $pdf->GetY() + 8;
-                $pdf->SetY(120);
-                $pdf->SetXY(10, $y);
-                $pdf->MultiCell(30, 16, utf8_decode("Total Horas : "),2, 'J');
-
-                $pdf->SetXY(23, $y);
-                $pdf->MultiCell(30, 16, $suma_total_horas,2, 'C');
-
-                $mensaje = "Para constancia de lo detallado anteriormente, se mantiene un archivo con el expediente de PPP del estudiante, mismo que reposa en la secretaria de la Coordinación de la Carrera.";
-                $pdf->Write(5, utf8_decode($mensaje));
-
-                $pdf->Output('', 'certificado_ppp_empresarial_'.date("d_m_Y_H_i").'.pdf');
+            }else{
+                $getAlumno= $this->model->selectCertificadoAlumno($_SESSION['user_data']['id_usuario']);
+                $date_du_jour = fechaCastellano(date("d-m-Y"));
+                $data[0] = $getAlumno;
+                $data[0]['tipo']='EMPRESARIAL';
+                $data[1] = $date_du_jour;
+                $data[3] = $this->model->selectCertificadoAlumnoDetalle($_SESSION['user_data']['id_usuario'],1);
+                $html2pdf = new Html2Pdf('P', 'A4', 'es', 'true', 'UTF-8');
+                ob_end_clean();
+                $html = getFile('template/reporte/reporte_ppp_general',$data);
+                $html2pdf->setDefaultFont('times');
+                $html2pdf->pdf->SetTitle('Certificado practicas pre-profesionales');
+                $html2pdf->writeHTML($html);
+                $html2pdf->output('certificado_ppp_empresarial_'.date("d_m_Y_H_i").'.pdf');
             }
 
+        }
+    }
+    
+    public function certificadopppcomunitario($int_id_alumno = 0){
+        if (empty($_SESSION['permisos_modulo']['r'])) {
+            header('location:'.server_url.'Errors');
+        }else{
+            if($_SESSION['user_data']['id_rol'] != 2){
+                $int_id_alumno = Intval(strclean($int_id_alumno));
+                if($int_id_alumno == 0){
+                    header('location:'.server_url.'Errors');
+                }else{
+                    $getAlumno= $this->model->selectCertificadoAlumno($int_id_alumno);
+                    $date_du_jour = fechaCastellano(date("d-m-Y"));
+                    $data[0] = $getAlumno;
+                    $data[0]['tipo']='SERVICIO A LA COMUNIDAD';
+                    $data[1] = $date_du_jour;
+                    $data[3] = $this->model->selectCertificadoAlumnoDetalle($int_id_alumno,2);
+                    $html2pdf = new Html2Pdf('P', 'A4', 'es', 'true', 'UTF-8');
+                    ob_end_clean();
+                    $html = getFile('template/reporte/reporte_ppp_general',$data);
+                    $html2pdf->setDefaultFont('times');
+                    $html2pdf->pdf->SetTitle('Certificado practicas pre-profesionales');
+                    $html2pdf->writeHTML($html);
+                    $html2pdf->output('certificado_ppp_comunitario_'.date("d_m_Y_H_i").'.pdf');
+                }
+            }else{
+                $getAlumno= $this->model->selectCertificadoAlumno($_SESSION['user_data']['id_usuario']);
+                $date_du_jour = fechaCastellano(date("d-m-Y"));
+                $data[0] = $getAlumno;
+                $data[0]['tipo']='SERVICIO A LA COMUNIDAD';
+                $data[1] = $date_du_jour;
+                $data[3] = $this->model->selectCertificadoAlumnoDetalle($_SESSION['user_data']['id_usuario'],2);
+                $html2pdf = new Html2Pdf('P', 'A4', 'es', 'true', 'UTF-8');
+                ob_end_clean();
+                $html = getFile('template/reporte/reporte_ppp_general',$data);
+                $html2pdf->setDefaultFont('times');
+                $html2pdf->pdf->SetTitle('Certificado practicas pre-profesionales');
+                $html2pdf->writeHTML($html);
+                $html2pdf->output('certificado_ppp_comunitario_'.date("d_m_Y_H_i").'.pdf');
+            }
         }
     }
 
